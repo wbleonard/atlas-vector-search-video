29c188ce74138593d3ed6aa390c590364573fee1
Brian Leonard <brian.leonard@mongodb.com>
2022-11-29 17:01:22 -0500

    Delete params.py

--- params.py
+++
@@ -1,6 +0,0 @@
-# Input parameters
-
-#mongodb_conn_string = '<Application connection string of Atlas cluster>'
-mongodb_conn_string = "mongodb+srv://brian:mongodb@sa-shared-demo.lbvlu.mongodb.net/?retryWrites=true&w=majority"
-database = "VectorSearchDemo"
-collection = "video"
